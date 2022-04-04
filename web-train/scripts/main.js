bdf1814fe37c9d2f1ae74bc39fe40a2c7508648f
umax1120 <102953027+umax1120@users.noreply.github.com>
2022-04-04 17:41:44 +0900

    Add files via upload

---
+++ web-train/scripts/main.js
@@ -0,0 +1,38 @@
+//画像の切り替え
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/shinomiyakoh.jpg') {
+      myImage.setAttribute('src','images/学生証.png');
+    } else {
+      myImage.setAttribute('src','images/shinomiyakoh.jpg');
+    }
+}
+
+
+//挨拶メッセージ
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Welcome, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome、' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
+  
